8a41d4aac1cc16f0992b48dce35c585c12e2d509
xiaowuzi2012 <451968472@qq.com>
2018-03-05 01:02:42 +0800

    7-20 购物车小球动画实现（3）
    7-20 购物车小球动画实现（3）

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -28,7 +28,7 @@
                   <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
                 </div>
                 <div class="cartcontrol-wrapper">
-                  <cartcontrol :food="food"></cartcontrol>
+                  <cartcontrol @add="addFood" :food="food"></cartcontrol>
                 </div>
               </div>
             </li>
@@ -36,7 +36,7 @@
         </li>
       </ul>
     </div>
-    <shopcart :selectFoods="selectFoods" :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice"></shopcart>
+    <shopcart ref="shopcart" :selectFoods="selectFoods" :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice"></shopcart>
   </div>
 </template>
 <script>
@@ -124,6 +124,15 @@
           height += item.clientHeight;
           this.listHeight.push(height);
         }
+      },
+      addFood(target) {
+        this._drop(target);
+      },
+      _drop(target) {
+        // 体验优化,异步执行下落动画
+        this.$nextTick(() => {
+          this.$refs.shopcart.drop(target);
+        });
       }
     },
     components: {
