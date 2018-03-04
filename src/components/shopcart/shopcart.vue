0cc3af5905b7e03e100f1a790a02d6d0f90b31a3
xiaowuzi2012 <451968472@qq.com>
2018-03-05 02:22:12 +0800

    7-22 购物车详情页实现（2）
    遗留问题：131行this.fold报错

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="shopcart">
-    <div class="content">
+    <div class="content" @click="toggleList">
       <div class="content-left">
         <div class="logo-wrapper">
           <div class="logo" :class="{'highlight':totalCount>0}">
@@ -24,9 +24,31 @@
         </transition>
       </div>
     </div>
+    <transition name="fold">
+      <div class="shopcart-list" v-show="listShow">
+        <div class="list-header">
+          <h1 class="title">购物车</h1>
+          <span class="empty">清空</span>
+        </div>
+        <div class="list-content">
+          <ul>
+            <li class="food" v-for="(food, index) in selectFoods" :key="index">
+              <span class="name">{{food.name}}</span>
+              <div class="price">
+                <span>￥{{food.price*food.count}}</span>
+              </div>
+              <div class="cartcontrol-wrapper">
+                <cartcontrol :food="food"></cartcontrol>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 <script>
+  import cartcontrol from 'components/cartcontrol/cartcontrol';
   export default {
     props: {
       selectFoods: {
@@ -68,7 +90,8 @@
             show: false
           }
         ],
-        dropBalls: []
+        dropBalls: [],
+        fold: true
       };
     },
     computed: {
@@ -101,6 +124,15 @@
           return 'not-enough';
         } else {
           return 'enough';
+        }
+      },
+      listShow() {
+        if (!this.totalCount) {
+          this.fold = true;
+          return false;
+        } else {
+          let show = !this.fold;
+          return show;
         }
       }
     },
@@ -154,11 +186,21 @@
           ball.show = false;
           el.style.display = 'none';
         }
+      },
+      toggleList() {
+        if (!this.totalCount) {
+          return;
+        }
+        this.fold = !this.fold;
       }
+    },
+    components: {
+      cartcontrol
     }
   };
 </script>
 <style  lang='stylus' rel='stylesheet/stylus'>
+@import "../../common/stylus/mixin.styl"
 .shopcart
   position: fixed
   left: 0px
@@ -258,4 +300,55 @@
         border-radius: 50%
         background: rgb(0, 160, 220)
         transition: all .4s linear
+  .shopcart-list
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    z-index: -1
+    transform: translate3d(0, -100%, 0)
+    &.fold-enter-active, &.fold-leave-active
+      transition: all 0.5s
+    &.fold-enter, &.fold-leave-active
+      transform: translate3d(0, 0, 0)
+    .list-header
+      height: 40px
+      line-height: 40px
+      padding: 0 18px
+      background: #f3f5f7
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+      .title
+        float: left
+        font-size: 14px
+        color: rgb(7, 17, 27)
+      .empty
+        float: right
+        font-size: 12px
+        color: rgb(0, 160, 220)
+    .list-content
+      padding: 0 18px
+      max-height: 217px
+      overflow: hidden
+      background: #fff
+      .food
+        position: relative
+        padding: 12px 0
+        box-sizing: border-box
+        border-1px(rgba(7, 17, 27, 0.1))
+        .name
+          line-height: 24px
+          font-size: 14px
+          color: rgb(7, 17, 27)
+        .price
+          position: absolute
+          right: 90px
+          bottom: 12px
+          line-height: 24px
+          font-size: 14px
+          font-weight: 700
+          color: rgb(240, 20, 20)
+        .cartcontrol-wrapper
+          position: absolute
+          right: 0
+          bottom: 6px
 </style>
